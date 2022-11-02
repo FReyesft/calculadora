07d05e9e238ca69987909dbe14d35e03ebb84c5a
FReyeSFT <fernandoreyezpinto@gmail.com>
2022-11-01 20:42:30 -0500

    separe los valores en dos <p> para poder facilitar hacer las operaciones

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const botonesOperadores = document.querySelectorAll(".operador");
 const botonNumeros = document.querySelectorAll(".numero");
+const botonBorrar = document.querySelector(".borrar");
+const primerValor = document.querySelector(".primer-valor");
+const segundoValor = document.querySelector(".segundo-valor");
+
 botonesOperadores.forEach((boton) => {
     boton.addEventListener("click", () => {
-        alert(boton.value)    
-    })
+        primerValor.textContent = primerValor.textContent + boton.value;
+        if(primerValor.textContent.includes('/', 'x', '-', '+')){
+            let primerValorArray = primerValor.textContent.split("");
+            console.log(primerValorArray)
+        }
+    });
 })
+
+botonBorrar.addEventListener("click", () => {
+    let contenido = primerValor.textContent;
+    let contenido2 = segundoValor.textContent;
+    if (contenido.length > 0) {
+        primerValor.textContent = "";
+    }
+    if (contenido2.length > 0) {
+        segundoValor.textContent = "";
+    }
+});
+
 
 botonNumeros.forEach((numero) => {
     numero.addEventListener("click", () => {
-        alert(numero.value)
-    })
+        primerValor.textContent = primerValor.textContent + numero.value;
+    });
 })
